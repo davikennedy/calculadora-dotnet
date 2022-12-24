d9f2309f5af2c45a38c7d06026863e421d5943b6
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-12-23 23:33:37 -0300

    Commit inicial

---
+++ Calculadora/Calculadora/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Write("Primeiro valor: ");
+            float valor1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine(valor1);
+        }
+    }
+}
