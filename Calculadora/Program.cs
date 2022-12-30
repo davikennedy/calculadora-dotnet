daacde467d20820b499893a82f5c857d8809dfae
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-12-29 23:39:55 -0300

    Adicionando operação de Soma

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -7,10 +7,15 @@ namespace Calculadora
         static void Main(string[] args)
         {
             Console.Clear();
+
             Console.Write("Primeiro valor: ");
             float valor1 = float.Parse(Console.ReadLine());
 
-            Console.WriteLine(valor1);
+            Console.Write("Segundo valor: ");
+            float valor2 = float.Parse(Console.ReadLine());
+
+            float resultado = valor1 + valor2;
+            Console.WriteLine($"\nO resultado da soma é {resultado}");
         }
     }
 }
