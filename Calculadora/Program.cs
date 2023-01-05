18f97c1a4db591e69f3ba8fc98204226ee83b344
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-01-04 22:38:22 -0300

    Refatorando projeto e adicionando operação de Subtração

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -6,6 +6,11 @@ namespace Calculadora
     {
         static void Main(string[] args)
         {
+            Subtracao();
+        }
+
+        static void Soma() 
+        {
             Console.Clear();
 
             Console.Write("Primeiro valor: ");
@@ -16,6 +21,21 @@ namespace Calculadora
 
             float resultado = valor1 + valor2;
             Console.WriteLine($"\nO resultado da soma é {resultado}");
+        }
+
+        static void Subtracao()
+        {
+            Console.Clear();
+
+            Console.Write("Primeiro valor: ");
+            float valor1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Segundo valor: ");
+            float valor2 = float.Parse(Console.ReadLine());
+
+            float resultado = valor1 - valor2;
+            Console.WriteLine($"\nO resultado da subtração é {resultado}");
+            Console.ReadKey();
         }
     }
 }
