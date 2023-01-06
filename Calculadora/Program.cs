a07aa9e0693d3ca41648ea9d4ec74db7ee9cff21
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-01-05 23:44:20 -0300

    Criando Menu da aplicação

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -6,7 +6,42 @@ namespace Calculadora
     {
         static void Main(string[] args)
         {
-            Multiplicacao();
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine
+            (
+                "Que operação deseja realizar?\n" +
+                "1 - Soma\n" +
+                "2 - Subtração\n" +
+                "3 - Divisão\n" +
+                "4 - Multiplicação"
+            );
+
+            Console.Write("\nSelecione uma opção: ");
+            short opcao = short.Parse(Console.ReadLine());
+
+            switch (opcao)
+            {
+                case 1:
+                    Soma();
+                    break;
+                case 2:
+                    Subtracao();
+                    break;
+                case 3:
+                    Divisao();
+                    break;
+                case 4:
+                    Multiplicacao();
+                    break;
+                default:
+                    Menu();
+                    break;
+            }
         }
 
         static void Soma() 
@@ -21,6 +56,8 @@ namespace Calculadora
 
             float resultado = valor1 + valor2;
             Console.WriteLine($"\nO resultado da soma é {resultado}");
+            Console.ReadKey();
+            Menu();
         }
 
         static void Subtracao()
@@ -36,6 +73,7 @@ namespace Calculadora
             float resultado = valor1 - valor2;
             Console.WriteLine($"\nO resultado da subtração é {resultado}");
             Console.ReadKey();
+            Menu();
         }
 
         static void Divisao()
@@ -51,6 +89,7 @@ namespace Calculadora
             float resultado = valor1 / valor2;
             Console.WriteLine($"\nO resultado da divisão é {resultado}");
             Console.ReadKey();
+            Menu();
         }
 
         static void Multiplicacao()
@@ -67,6 +106,7 @@ namespace Calculadora
 
             Console.WriteLine($"\nO resultado da multiplicação é {resultado}");
             Console.ReadKey();
+            Menu();
         }
     }
 }
